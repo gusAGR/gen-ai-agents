f659c3094b8fdec6dc6f8b7d964f2438751e7655
gusAGR <gustavo.a.garcia.rod@outlook.com>
2025-10-16 11:12:54 +0200

    Simple langgraph agent

---
+++ langgraph-intro/langgraph-intro.py
@@ -0,0 +1,29 @@
+from typing import TypedDict, List
+from langgraph.graph import StateGraph, START, END
+
+class State(TypedDict):
+    graph_state: str
+
+def node_1(state: State):
+    return {"graph_state": state['graph_state'] + " Updated once."}
+
+
+def node_2(state: State):
+    return {"graph_state": state['graph_state'] + " Updated twice."}
+
+
+def node_3(state: State):
+    return {"graph_state": state['graph_state'] + " Updated 3 times."}
+
+builder = StateGraph(State)
+
+builder.add_node(node_1)
+builder.add_node(node_2)
+builder.add_node(node_3)
+
+builder.add_edge(START, "node_1")
+builder.add_edge("node_1", "node_2")
+builder.add_edge("node_2", "node_3")
+builder.add_edge("node_3", END)
+
+graph = builder.compile()
